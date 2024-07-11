1375ed88d55c0f922c537fd6da000bd809ab5b64
emonhaque1998 <emonhaque.net@gmail.com>
2024-07-11 20:59:00 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,27 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [showHeader, setHeader] = useState(false);
+
   const menuShowHandeler = () => {
     setHeader(!showHeader);
   };
+
+  const getData = async () => {
+    try {
+      const res = await fetch("/api/hello", {
+        method: "GET",
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <div className="bg-gradient-to-r from-rose-100 to-teal-100 h-screen">
@@ -15,38 +33,38 @@ export default function Home() {
               <a href="">Logo</a>
             </div>
             <div
-              style={{ top: showHeader ? "100%" : "-200px" }}
+              style={{ top: showHeader ? "100%" : "-300px" }}
               className={`max-lg:absolute max-lg:${
                 showHeader ? "opacity-1" : "opacity-1"
-              } max-lg:transition-all max-lg:bg-white max-lg:w-full max-lg:left-0 max-lg:px-5 max-lg:py-2`}
+              } max-lg:transition-all max-lg:bg-white max-lg:w-full max-lg:left-0 `}
             >
               <ul className="lg:flex lg:gap-5">
-                <li>
+                <li className="hover:bg-black hover:text-white max-lg:py-1 max-lg:px-5 hover:cursor-pointer">
                   <a href="" className="font-nav font-bold">
                     Home
                   </a>
                 </li>
-                <li>
+                <li className="hover:bg-black hover:text-white max-lg:py-1 max-lg:px-5 hover:cursor-pointer">
                   <a href="" className="font-nav font-bold">
                     About
                   </a>
                 </li>
-                <li>
+                <li className="hover:bg-black hover:text-white max-lg:py-1 max-lg:px-5 hover:cursor-pointer">
                   <a href="" className="font-nav font-bold">
                     Portfolio
                   </a>
                 </li>
-                <li>
+                <li className="hover:bg-black hover:text-white max-lg:py-1 max-lg:px-5 hover:cursor-pointer">
                   <a href="" className="font-nav font-bold">
                     Service
                   </a>
                 </li>
-                <li>
+                <li className="hover:bg-black hover:text-white max-lg:py-1 max-lg:px-5 hover:cursor-pointer">
                   <a href="" className="font-nav font-bold">
                     News
                   </a>
                 </li>
-                <li>
+                <li className="hover:bg-black hover:text-white max-lg:py-2 max-lg:px-5 hover:cursor-pointer">
                   <a href="" className="font-nav font-bold">
                     Contact
                   </a>
