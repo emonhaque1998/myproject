b80d2c5920ed72bb22a857441cdae3c0b00cb788
unknown <emonhaque.net@gmail.com>
2024-07-31 15:37:07 +0600

    update login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -171,7 +171,7 @@ export default function Login() {
               </div>
               <div className="flex flex-col gap-2">
                 <button
-                  type="submit"
+                  onClick={() => signIn("google")}
                   className={`bg-sky-300/20 flex justify-center items-center gap-2 ${
                     theme ? "text-black" : "text-white"
                   } max-md:text-sm focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
